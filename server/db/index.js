846075e2542d60e15a72f508e8a04aca9b51606a
Ciaran Martin Conners <ciaranconners@gmail.com>
2017-07-15 18:24:05 -0400

    ...

--- server/db/index.js
+++ server/db/index.js
@@ -2,14 +2,18 @@ var Sequelize = require('sequelize');
 var db = new Sequelize(process.env.RDS_CONNECTION_URL);
 
 var User = db.define('User', {
-	username: Sequelize.STRING,
-	id: { type: Sequelize.INTEGER, primaryKey: true },
-	upvote_count: Sequelize.INTEGER,
-	downvote_count: Sequelize.INTEGER,
-	neutral_count: Sequelize.INTEGER
+  username: Sequelize.STRING,
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  upvote_count: Sequelize.INTEGER,
+  downvote_count: Sequelize.INTEGER,
+  neutral_count: Sequelize.INTEGER
 });
 
 var Url = db.define('Url', {
+<<<<<<< HEAD
 	id: { type: Sequelize.INTEGER, primaryKey: true },
 	id_category: { type: Sequelize.INTEGER,
    	references: {
@@ -21,65 +25,102 @@ var Url = db.define('Url', {
 	upvote_count: Sequelize.INTEGER,
 	downvote_count: Sequelize.INTEGER,
 	neutral_count: Sequelize.INTEGER
+=======
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  id_category: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: Category,
+      key: 'id'
+    }
+  },
+  id_category: Sequelize.STRING,
+  upvote_count: Sequelize.INTEGER,
+  downvote_count: Sequelize.INTEGER,
+  neutral_count: Sequelize.INTEGER
+>>>>>>> ...
 });
 
 var Category = db.define('Category', {
-	id: { type: Sequelize.INTEGER, primaryKey: true },
-	name: Sequelize.STRING,
-	id_parent: { type: Sequelize.INTEGER,
-   	references: {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  name: Sequelize.STRING,
+  id_parent: {
+    type: Sequelize.INTEGER,
+    references: {
       model: Category,
       key: 'id'
-		}
-	}
+    }
+  }
 });
 
 var Url_vote = db.define('Url_vote', {
-	id: { type: Sequelize.INTEGER, primaryKey: true },
-	id_user: { type: Sequelize.INTEGER,
-   	references: {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  id_user: {
+    type: Sequelize.INTEGER,
+    references: {
       model: User,
       key: 'id'
-		}
-	},
-	id_url: { type: Sequelize.INTEGER,
-   	references: {
+    }
+  },
+  id_url: {
+    type: Sequelize.INTEGER,
+    references: {
       model: Url,
       key: 'id'
-		}
-	},
-	type: Sequelize.STRING
+    }
+  },
+  type: Sequelize.STRING
 });
 
 var Comment = db.define('Comment', {
-	id: { type: Sequelize.INTEGER, primaryKey: true },
-	id_url: { type: Sequelize.INTEGER,
-   	references: {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  id_url: {
+    type: Sequelize.INTEGER,
+    references: {
       model: Url,
       key: 'id'
-		}
-	},
-	id_user: { type: Sequelize.INTEGER,
-   	references: {
+    }
+  },
+  id_user: {
+    type: Sequelize.INTEGER,
+    references: {
       model: User,
       key: 'id'
-		}
-	},
-	id_parent: { type: Sequelize.INTEGER,
-   	references: {
+    }
+  },
+  id_parent: {
+    type: Sequelize.INTEGER,
+    references: {
       model: Comment,
       key: 'id'
-		}
-	},
-	text: Sequelize.STRING
+    }
+  },
+  text: Sequelize.STRING
 });
 
 var Comment_vote = db.define('Comment_vote', {
-	id: { type: Sequelize.INTEGER, primaryKey: true },
-	id_comment: { type: Sequelize.INTEGER,
-   	references: {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  id_comment: {
+    type: Sequelize.INTEGER,
+    references: {
       model: Comment,
       key: 'id'
+<<<<<<< HEAD
 		}
 	},
 	id_user: { type: Sequelize.INTEGER,
@@ -89,3 +130,15 @@ var Comment_vote = db.define('Comment_vote', {
 		}
 	}
 });
+=======
+    }
+  },
+  id_user: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: User,
+      key: 'id'
+    }
+  }
+});
+>>>>>>> ...
